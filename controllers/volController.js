9b9329df3aea0e2ac7da81459daad12252fa4d55
Alex Merced - AlexMercedCoder.com <alexmerced@alexmerced.com>
2019-11-06 10:42:56 -0500

    Added the ability to assign volunteers

--- controllers/volController.js
+++ controllers/volController.js
@@ -5,6 +5,7 @@ module.exports = router;
 
 //Schema
 const Volunteers = require('../models/vol.js');
+const Events = require('../models/events.js');
 
 /////////////////////
 //Index Routes
@@ -115,5 +116,53 @@ router.put('/edit/:indexOf', (req, res) => {
     else {res.redirect('/vol/error')}}
         else{
             res.redirect('/vol/error');
+        }
+});
+
+/////////////////////
+//Assign Volunteers
+/////////////////////
+
+router.get('/assign/:event', (req, res) => {
+    if(req.session){
+        console.log(req.session);
+    if (req.session.login === true){
+        Volunteers.find({username: req.session.user}, (error, data)=>{
+        console.log(data);
+        res.render('./vol/assign.ejs', {
+            data: data,
+            event: req.params.event
+        });
+    });} else {res.redirect('/vol/error')}}
+    else{
+        res.redirect('/vol/error');
+    }
+});
+
+router.post('/assign/:indexOf', (req, res) => {
+    //Check if user logged in
+    if(req.session){
+    if (req.session.login === true){
+    req.body.username = req.session.user;
+    console.log(req.body);
+    //check for each request object that is 'on' and add key to name array
+    const bodyKeys = Object.keys(req.body);
+    console.log(bodyKeys);
+    let nameArray = [];
+    for (key of bodyKeys){
+        console.log(key,req.body[key]);
+        if (req.body[key] === 'on'){
+            nameArray.push(key);
+        }
+        console.log(nameArray)
+    }
+    updateObj = {volunteers: nameArray}
+    //set name array as volunteer property of event
+    Events.findByIdAndUpdate(req.params.indexOf, updateObj, {new:true}, (err, updatedModel)=>{
+        res.redirect('/events/');
+    });}
+    else {res.redirect('/events/error')}}
+        else{
+            res.redirect('/events/error');
         }
 });
